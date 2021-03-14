e84cd80c817628d2f17772f8319788a205e5bc67
achariso <thanasis.harisoudis@gmail.com>
2021-03-15 01:00:43 +0200

    split train_pgpg.py into train_setup.py and a train_pgpg.py

--- src/train_pgpg.py
+++ src/train_pgpg.py
@@ -1,8 +1,3 @@
-import os
-import sys
-
-import torch
-from IPython import get_ipython
 from IPython.core.display import display
 from torch import Tensor
 # noinspection PyProtectedMember
@@ -10,51 +5,10 @@ from torch.utils.data import DataLoader
 
 from datasets.deep_fashion import ICRBDataset, ICRBCrossPoseDataloader
 from modules.pgpg import PGPG
+from train_setup import run_locally, exec_device, log_level, datasets_groot, models_groot
 from utils.dep_free import get_tqdm, in_notebook
-from utils.filesystems.gdrive.colab import ColabFilesystem, ColabFolder, ColabCapsule
-from utils.filesystems.gdrive.remote import GDriveCapsule, GDriveFilesystem, GDriveFolder
-from utils.filesystems.local import LocalFilesystem, LocalFolder, LocalCapsule
 from utils.ifaces import FilesystemDataset
 from utils.metrics import GanEvaluator
-# Flag to run first test batches locally
-from utils.plot import ensure_matplotlib_fonts_exist
-
-run_locally = True
-if in_notebook():
-    run_locally = False  # local runs are performed vis IDE runs (and thus terminal)
-
-# Check if running inside Colab or Kaggle
-if 'google.colab' in sys.modules or 'google.colab' in str(get_ipython()) or 'COLAB_GPU' in os.environ:
-    exec_env = 'colab'
-    local_gdrive_root = '/content/drive/MyDrive'
-    run_locally = False
-elif 'KAGGLE_KERNEL_RUN_TYPE' in os.environ:
-    exec_env = 'kaggle'
-    local_gdrive_root = '/kaggle/working/GoogleDrive'
-    run_locally = False
-else:
-    exec_env = 'ssh'
-    local_gdrive_root = '/home/achariso/PycharmProjects/gans-thesis/.gdrive'
-    if not os.path.exists(local_gdrive_root):
-        local_gdrive_root = input('local_gdrive_root = ')
-        run_locally = False
-assert os.path.exists(local_gdrive_root), f'local_gdrive_root={local_gdrive_root} NOT FOUND'
-
-# Check if GPU is available
-exec_device = torch.device('cuda' if torch.cuda.is_available() and not run_locally else 'cpu')
-
-# Get log level
-global log_level
-if in_notebook():
-    try:
-        log_level = f'{log_level}'
-    except NameError:
-        print('You should define log_level variable before running this script')
-        log_level = input('log_level=')
-        assert log_level in ['debug', 'info', 'warning', 'error']
-else:
-    log_level = 'info' if not run_locally else 'debug'
-print(f'log_level={log_level}')
 
 ###################################
 ###  Hyper-parameters settings  ###
@@ -78,40 +32,6 @@ skip_pose_norm = True
 #   - PGPG config file
 pgpg_config_id = f'{target_shape}_MSE_256_6_4_5_none_none_1e4_true_false_false'  # as proposed in the original paper
 
-##########################################
-###  GDrive Filesystem Initialization  ###
-##########################################
-#   - define FilesystemFolder to interact with files/folders under the root folder on Google Drive
-if exec_env == 'colab':
-    # Colab filesystem is a locally-mounted filesystem. Interacts with native OS calls.
-    fs = ColabFilesystem(ccapsule=ColabCapsule())
-    groot = ColabFolder.root(capsule_or_fs=fs)
-elif run_locally:
-    # Local filesystem (basically one directory under given root). Interacts with native OS calls.
-    fs = LocalFilesystem(ccapsule=LocalCapsule(local_root=local_gdrive_root))
-    groot = LocalFolder.root(capsule_or_fs=fs)
-else:
-    # Remove filesystem. Interacts via GoogleDrive API calls.
-    global use_refresh_token
-    try:
-        use_refresh_token = use_refresh_token or False
-    except NameError:
-        use_refresh_token = run_locally
-    gcapsule = GDriveCapsule(local_gdrive_root=local_gdrive_root, use_http_cache=True, update_credentials=True,
-                             use_refresh_token=use_refresh_token)
-    fs = GDriveFilesystem(gcapsule=gcapsule)
-    groot = GDriveFolder.root(capsule_or_fs=fs, update_cache=True)
-#   - define immediate sub-folders of root folder
-# print(json.dumps(groot.subfolders, indent=4))
-datasets_groot = groot.subfolder_by_name('Datasets')
-models_groot = groot.subfolder_by_name('Models')
-fonts_groot = groot.subfolder_by_name('Fonts')
-#   - ensure that system and matplotlib fonts directories exist and have the correct font files
-rebuilt_fonts = ensure_matplotlib_fonts_exist(fonts_groot, force_rebuild=False)
-if rebuilt_fonts and exec_env != 'ssh':
-    groot.fs.logger.critical('Fonts rebuilt! Terminating python process now.')
-    os.kill(os.getpid(), 9)
-
 ###################################
 ###   Dataset Initialization    ###
 ###################################
@@ -134,7 +54,7 @@ elif hasattr(dataset, 'dataset') and isinstance(dataset.dataset, FilesystemDatas
     dataset.dataset.fetch_and_unzip(in_parallel=False, show_progress=True)
 else:
     raise TypeError('dataset must implement utils.ifaces.FilesystemDataset in order to be auto-downloaded and unzipped')
-#   - apply basic tests
+#   - apply rudimentary tests
 assert issubclass(dataloader.__class__, DataLoader)
 assert len(dataloader) == len(dataset) // batch_size + (1 if len(dataset) % batch_size else 0)
 _image_1, _image_2, _dense_pose_2 = next(iter(dataloader))
---
+++ src/train_setup.py
@@ -0,0 +1,90 @@
+import os
+import sys
+
+import torch
+from IPython import get_ipython
+
+from utils.dep_free import in_notebook
+from utils.filesystems.gdrive.colab import ColabFilesystem, ColabFolder, ColabCapsule
+from utils.filesystems.gdrive.remote import GDriveCapsule, GDriveFilesystem, GDriveFolder
+from utils.filesystems.local import LocalFilesystem, LocalFolder, LocalCapsule
+# Flag to run first test batches locally
+from utils.plot import ensure_matplotlib_fonts_exist
+
+##########################################
+###     Environment Initialization     ###
+##########################################
+run_locally = True
+if in_notebook():
+    run_locally = False  # local runs are performed vis IDE runs (and thus terminal)
+os.environ['TRAIN_ENV'] = 'local' if run_locally else 'nonlocal'
+
+# Check if running inside Colab or Kaggle
+if 'google.colab' in sys.modules or 'google.colab' in str(get_ipython()) or 'COLAB_GPU' in os.environ:
+    exec_env = 'colab'
+    local_gdrive_root = '/content/drive/MyDrive'
+    run_locally = False
+elif 'KAGGLE_KERNEL_RUN_TYPE' in os.environ:
+    exec_env = 'kaggle'
+    local_gdrive_root = '/kaggle/working/GoogleDrive'
+    run_locally = False
+else:
+    exec_env = 'ssh'
+    local_gdrive_root = '/home/achariso/PycharmProjects/gans-thesis/.gdrive'
+    if not os.path.exists(local_gdrive_root):
+        local_gdrive_root = input('local_gdrive_root = ')
+        run_locally = False
+assert os.path.exists(local_gdrive_root), f'local_gdrive_root={local_gdrive_root} NOT FOUND'
+os.environ['TRAIN_EXEC_ENV'] = exec_env
+
+# Check if GPU is available
+exec_device = torch.device('cuda' if torch.cuda.is_available() and not run_locally else 'cpu')
+os.environ['TRAIN_EXEC_DEV'] = str(exec_device)
+
+# Get log level
+global log_level
+if in_notebook():
+    try:
+        log_level = f'{log_level}'
+    except NameError:
+        print('You should define log_level variable before running this script')
+        log_level = input('log_level=')
+        assert log_level in ['debug', 'info', 'warning', 'error']
+else:
+    log_level = 'info' if not run_locally else 'debug'
+os.environ['TRAIN_LOG_LEVEL'] = log_level
+print(f'log_level={log_level}')
+
+##########################################
+###  GDrive Filesystem Initialization  ###
+##########################################
+#   - define FilesystemFolder to interact with files/folders under the root folder on Google Drive
+if exec_env == 'colab':
+    # Colab filesystem is a locally-mounted filesystem. Interacts with native OS calls.
+    fs = ColabFilesystem(ccapsule=ColabCapsule())
+    groot = ColabFolder.root(capsule_or_fs=fs)
+elif run_locally:
+    # Local filesystem (basically one directory under given root). Interacts with native OS calls.
+    fs = LocalFilesystem(ccapsule=LocalCapsule(local_root=local_gdrive_root))
+    groot = LocalFolder.root(capsule_or_fs=fs)
+else:
+    # Remove filesystem. Interacts via GoogleDrive API calls.
+    global use_refresh_token
+    try:
+        use_refresh_token = use_refresh_token or False
+    except NameError:
+        use_refresh_token = run_locally
+    gcapsule = GDriveCapsule(local_gdrive_root=local_gdrive_root, use_http_cache=True, update_credentials=True,
+                             use_refresh_token=use_refresh_token)
+    fs = GDriveFilesystem(gcapsule=gcapsule)
+    groot = GDriveFolder.root(capsule_or_fs=fs, update_cache=True)
+#   - define immediate sub-folders of root folder
+# print(json.dumps(groot.subfolders, indent=4))
+datasets_groot = groot.subfolder_by_name('Datasets')
+models_groot = groot.subfolder_by_name('Models')
+fonts_groot = groot.subfolder_by_name('Fonts')
+#   - ensure that system and matplotlib fonts directories exist and have the correct font files
+rebuilt_fonts = ensure_matplotlib_fonts_exist(fonts_groot, force_rebuild=False)
+if rebuilt_fonts and exec_env != 'ssh':
+    groot.fs.logger.critical('Fonts rebuilt! Terminating python process now.')
+    os.kill(os.getpid(), 9)
