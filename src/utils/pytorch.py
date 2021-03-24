86c3d64813c2b6cdfb35a057da1c824e8d113a44
achariso <thanasis.harisoudis@gmail.com>
2021-03-24 23:21:44 +0200

    saved epoch 93 metrics

--- src/utils/pytorch.py
+++ src/utils/pytorch.py
@@ -14,6 +14,107 @@ from utils.command_line_logger import CommandLineLogger
 from utils.string import to_human_readable
 
 
+def corr(x: torch.Tensor, eps: float = 1e-08) -> torch.Tensor:
+    """
+    Compute the correlation matrix of given input vector $x$ using the unbiased sample correlation estimator.
+    (credits to mauricett, https://github.com/pytorch/pytorch/issues/19037#issuecomment-739002393)
+    :param x: the input vector of shape (N, n_vars)
+    :param eps: constant for numerical stability when normalizing $x$
+    :return: a torch.Tensor of shape (n_vars, n_vars)
+    """
+    n_samples, n_vars = tuple(x.shape)
+    x -= torch.mean(x, dim=0)
+    x /= torch.std(x, dim=0) + eps
+    print('x.shape', x.shape)
+    return 1 / (n_samples - 1) * x.transpose(-1, -2) @ x
+
+
+def cov(x: torch.Tensor) -> torch.Tensor:
+    """
+    Compute the covariance matrix of given input vector $x$ using the unbiased sample covariance estimator.
+    (credits to mauricett, https://github.com/pytorch/pytorch/issues/19037#issuecomment-739002393)
+    :param x: the input vector of shape (N, n_vars)
+    :return: a torch.Tensor of shape (n_vars, n_vars)
+    """
+    n_samples, n_vars = tuple(x.shape)
+    x -= torch.mean(x, dim=0)
+    return 1 / (n_samples - 1) * x.transpose(-1, -2) @ x
+
+
+def enable_verbose(model: nn.Module) -> None:
+    """
+    Register verbose hooks on model's forward pass to output the shape of each layer's output tensor.
+    :param (nn.Module) model: an torch.nn.Module instance
+    """
+    logger = model.logger if hasattr(model, 'logger') else \
+        CommandLineLogger(log_level=os.getenv('TRAIN_LOG_LEVEL', 'debug'), name='ModelVerbose')
+    # Register a hook for each layer
+    for _name, _layer in model.named_children():
+        _layer.register_forward_hook(lambda _l, _, _out: logger.debug(f"{_name}: {_out.shape}"))
+
+
+def get_gradient(disc: nn.Module, real: torch.Tensor, fake: torch.Tensor, epsilon: torch.Tensor) -> torch.Tensor:
+    """
+    Return the gradient of the critic's scores w.r.t. mixes of real and fake images.
+    Attention: Returns the gradient of c(x) w.r.t. to x. If for example x is a (3,128,128) image, then grad_x(c(x)) will
+               be a (3,128,128) tensor containing how much the c(x) changes if x[i][j][k] changes.
+    :param (nn.Module) disc: the critic model
+    :param (torch.Tensor) real: a batch of real images
+    :param (torch.Tensor) fake: a batch of fake images
+    :param (torch.Tensor) epsilon: a vector of the uniformly random proportions of real/fake per mixed image
+    :return: (torch.Tensor) the gradient of the critic's scores, with respect to the mixed image
+    """
+    # Mix the images together
+    mixed_images = real * epsilon + fake * (1 - epsilon)
+
+    # Calculate the critic's scores on the mixed images
+    mixed_scores = disc(mixed_images)
+
+    # Take the gradient of the scores with respect to the images
+    return torch.autograd.grad(
+        # Note: We need to take the gradient of outputs with respect to inputs.
+        # https://pytorch.org/docs/stable/autograd.html#torch.autograd.grad
+        inputs=mixed_images,
+        outputs=mixed_scores,
+        # These other parameters have to do with how the pytorch autograd engine works
+        grad_outputs=torch.ones_like(mixed_scores),
+        create_graph=True,
+        retain_graph=True,
+    )[0]
+
+
+def get_gradient_penalty_from_gradient(gradient: torch.Tensor) -> torch.Tensor:
+    """
+    Return the gradient penalty, given a gradient.
+    Given a batch of image gradients, we calculate the magnitude of each image's gradient and penalize the mean
+    quadratic distance of each magnitude to 1.
+    :param (torch.Tensor) gradient: the gradient of the critic's scores, with respect to the mixed image
+    :return: a scalar torch.Tensor object containing the gradient penalty
+    """
+    # Flatten the gradients so that each row captures one image
+    gradient = gradient.view(len(gradient), -1)
+    # Calculate the magnitude of every row
+    gradient_norm = gradient.norm(2, dim=1)
+    # Penalize the mean squared distance of the gradient norms from 1
+    return torch.mean((torch.ones_like(gradient_norm) - gradient_norm) ** 2)
+
+
+def get_gradient_penalty(disc: nn.Module, real: torch.Tensor, fake: torch.Tensor, epsilon: torch.Tensor) \
+        -> torch.Tensor:
+    """
+    Get the gradient penalty regularization term, given the discriminator (critic) model, a set of real and fake and
+    images and the parameter :attr:`epsilon` to mix the two set of images (approximating an average input to critic).
+    :param (nn.Module) disc: the critic model
+    :param (torch.Tensor) real: a batch of real images
+    :param (torch.Tensor) fake: a batch of fake images
+    :param (torch.Tensor) epsilon: a vector of the uniformly random proportions of real/fake per mixed image
+    :return: (torch.Tensor) the gradient of the critic's scores, with respect to the mixed image
+    """
+    return get_gradient_penalty_from_gradient(
+        get_gradient(disc=disc, real=real, fake=fake, epsilon=epsilon)
+    )
+
+
 def get_total_params(model: Module, print_table: bool = False, sort_desc: bool = False) -> int or None:
     """
     Get total number of parameters from given nn.Module.
@@ -56,16 +157,32 @@ def get_total_params(model: Module, print_table: bool = False, sort_desc: bool =
     return total_count
 
 
-def enable_verbose(model: nn.Module) -> None:
+def invert_transforms(ts: transforms.Compose) -> transforms.Compose:
     """
-    Register verbose hooks on model's forward pass to output the shape of each layer's output tensor.
-    :param model: an torch.nn.Module instance
+    Inverts Normalize and ToTensor transforms in a transformation sequence defined via torchvision.transforms.Compose.
+    :param ts: transforms.Compose object with the list of transforms to be inverted
+    :return: a torchvision.transforms.Compose object with the last transforms inverted
     """
-    logger = model.logger if hasattr(model, 'logger') else \
-        CommandLineLogger(log_level=os.getenv('TRAIN_LOG_LEVEL', 'debug'), name='ModelVerbose')
-    # Register a hook for each layer
-    for _name, _layer in model.named_children():
-        _layer.register_forward_hook(lambda _l, _, _out: logger.debug(f"{_name}: {_out.shape}"))
+    inverse_transforms_list = []
+    for i in reversed(range(len(ts.transforms))):
+        t = ts.transforms[i]
+        if type(t) == transforms.Normalize:
+            inverse_transforms_list.append(UnNormalize(mean=t.mean, std=t.std, inplace=t.inplace))
+        elif type(t) == transforms.ToTensor:
+            # inverse_transforms_list.append(transforms.ToPILImage())
+            # We reached the ToTensor() transform; no point in continuing inverting upwards where the image was still a
+            # PIL image object
+            break
+    return transforms.Compose(inverse_transforms_list)
+
+
+def matrix_sqrt(mat: torch.Tensor) -> torch.Tensor:
+    """
+    Compute the square root of the given matrix.
+    :param mat: the input matrix as a torch.Tensor object
+    :return: a torch.Tensor fo the same shape as the input
+    """
+    return MatrixSquareRoot.apply(mat)
 
 
 class MatrixSquareRoot(Function):
@@ -103,73 +220,6 @@ class MatrixSquareRoot(Function):
         return grad_input
 
 
-def matrix_sqrt(mat: torch.Tensor) -> torch.Tensor:
-    """
-    Compute the square root of the given matrix.
-    :param mat: the input matrix as a torch.Tensor object
-    :return: a torch.Tensor fo the same shape as the input
-    """
-    return MatrixSquareRoot.apply(mat)
-
-
-def cov(x: torch.Tensor) -> torch.Tensor:
-    """
-    Compute the covariance matrix of given input vector $x$ using the unbiased sample covariance estimator.
-    (credits to mauricett, https://github.com/pytorch/pytorch/issues/19037#issuecomment-739002393)
-    :param x: the input vector of shape (N, n_vars)
-    :return: a torch.Tensor of shape (n_vars, n_vars)
-    """
-    n_samples, n_vars = tuple(x.shape)
-    x -= torch.mean(x, dim=0)
-    return 1 / (n_samples - 1) * x.transpose(-1, -2) @ x
-
-
-def corr(x: torch.Tensor, eps: float = 1e-08) -> torch.Tensor:
-    """
-    Compute the correlation matrix of given input vector $x$ using the unbiased sample correlation estimator.
-    (credits to mauricett, https://github.com/pytorch/pytorch/issues/19037#issuecomment-739002393)
-    :param x: the input vector of shape (N, n_vars)
-    :param eps: constant for numerical stability when normalizing $x$
-    :return: a torch.Tensor of shape (n_vars, n_vars)
-    """
-    n_samples, n_vars = tuple(x.shape)
-    x -= torch.mean(x, dim=0)
-    x /= torch.std(x, dim=0) + eps
-    print('x.shape', x.shape)
-    return 1 / (n_samples - 1) * x.transpose(-1, -2) @ x
-
-
-class UnNormalize(torch.nn.Module):
-    def __init__(self, mean, std, inplace=False):
-        super(UnNormalize, self).__init__()
-        self.mean = mean
-        self.std = std
-        self.inplace = inplace
-
-    def forward(self, tensor: torch.Tensor) -> torch.Tensor:
-        tensor = f.normalize(tensor, list(np.zeros_like(self.mean)), list(np.reciprocal(self.std)), self.inplace)
-        return f.normalize(tensor, list(-1 * np.asarray(self.mean)), list(np.ones_like(self.std)), self.inplace)
-
-
-def invert_transforms(ts: transforms.Compose) -> transforms.Compose:
-    """
-    Inverts Normalize and ToTensor transforms in a transformation sequence defined via torchvision.transforms.Compose.
-    :param ts: transforms.Compose object with the list of transforms to be inverted
-    :return: a torchvision.transforms.Compose object with the last transforms inverted
-    """
-    inverse_transforms_list = []
-    for i in reversed(range(len(ts.transforms))):
-        t = ts.transforms[i]
-        if type(t) == transforms.Normalize:
-            inverse_transforms_list.append(UnNormalize(mean=t.mean, std=t.std, inplace=t.inplace))
-        elif type(t) == transforms.ToTensor:
-            # inverse_transforms_list.append(transforms.ToPILImage())
-            # We reached the ToTensor() transform; no point in continuing inverting upwards where the image was still a
-            # PIL image object
-            break
-    return transforms.Compose(inverse_transforms_list)
-
-
 class ToTensorOrPass(transforms.ToTensor):
     """
     ToTensorOrPass Class:
@@ -196,3 +246,15 @@ class ToTensorOrPass(transforms.ToTensor):
             return (pic_or_tensor - tensor_min) / (tensor_max - tensor_min)
 
         return pic_or_tensor
+
+
+class UnNormalize(torch.nn.Module):
+    def __init__(self, mean, std, inplace=False):
+        super(UnNormalize, self).__init__()
+        self.mean = mean
+        self.std = std
+        self.inplace = inplace
+
+    def forward(self, tensor: torch.Tensor) -> torch.Tensor:
+        tensor = f.normalize(tensor, list(np.zeros_like(self.mean)), list(np.reciprocal(self.std)), self.inplace)
+        return f.normalize(tensor, list(-1 * np.asarray(self.mean)), list(np.ones_like(self.std)), self.inplace)
